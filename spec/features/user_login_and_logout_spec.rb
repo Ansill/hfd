7cfe70a8195ca2f362b30cdd8031cc81ac3e7967
William Holt <will.t.holt@gmail.com>
2015-08-31 21:21:37 -0400

    fixed failing spec

--- spec/features/user_login_and_logout_spec.rb
+++ spec/features/user_login_and_logout_spec.rb
@@ -9,21 +9,19 @@ feature "User logs in and logs out" do
     visit "/"
 
     visit new_user_session_path
-    expect(page).to have_css("h2", text: "Log in")
+    expect(page).to have_css("h2", text: "log in")
     expect(current_path).to eq(new_user_session_path)
 
-    fill_in "Email", with:"test@test.com"
-    fill_in "Password", with:"password"
+    fill_in "user_email", with:"test@test.com"
+    fill_in "user_password", with:"password"
 
     click_button "Log in"
 
     expect(current_path).to eq "/"
-    expect(page).to have_content "Signed in successfully"
 
     click_link "Log out"
 
     expect(current_path).to eq "/"
-    expect(page).to have_content "Signed out successfully"
 
   end
 
